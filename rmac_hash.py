cef088aaa2e65c334f645734cca068a907e74f5e
NOP <livint@posteo.de>
2020-11-17 03:56:18 +0100

    >

--- rmac_hash.py
+++ rmac_hash.py
@@ -12,12 +12,11 @@ hash_map = {}
 id_file_name = r'file_name.pkl'
 id_map = {}
 
-BIT_PAIRS = 12  # up to 12
+BITS = 24        # up to 12
 BATCH_COUNT = 5  # bits duplicater
 
-BITS = BIT_PAIRS * 2
-BATCH_HASH_COUNT = int(512 // BITS)
-BATCH_HASH_SIZE = BIT_PAIRS * BATCH_HASH_COUNT
+BATCH_HASH_COUNT = int(1024 // BITS)
+BATCH_HASH_SIZE = BITS * BATCH_HASH_COUNT
 
 
 # this generate a shuffled index to take out different combination of bits
@@ -59,22 +58,19 @@ def reset():
 random.seed(0)
 r_index = []
 for i in range(BATCH_COUNT):
-    r1 = list(range(512))
+    r1 = list(range(1024))
     random.shuffle(r1)
-    r_index.append(np.array(r1[:BATCH_HASH_SIZE], dtype=np.int32).reshape(BATCH_HASH_COUNT, BIT_PAIRS))
+    r_index.append(np.array(r1[:BATCH_HASH_SIZE], dtype=np.int32).reshape(BATCH_HASH_COUNT, BITS))
 r_index = np.concatenate(r_index)
-r_multi = [4 ** e for e in np.arange(BIT_PAIRS)]
+r_multi = [2 ** e for e in np.arange(BITS)]
 
 
 def generate_hashs(v):
-    s = np.empty((512,), dtype=np.int64)
-    for ii, b in enumerate(v):
-        if b < -0.035:
-            s[ii] = 0
-        elif b > 0.035:
-            s[ii] = 2
-        else:
-            s[ii] = 1
+    random.seed(0)
+    s = np.empty((1024,), dtype=np.int64)
+    maxv=len(v)
+    for ii in range(len(s)):
+        s[ii]=int(v[ii % maxv]<v[random.randint(maxv)])
     return (np.sum((s[r_index] * r_multi), axis=-1) << 8) + np.arange(len(r_index))
 
 
@@ -131,13 +127,13 @@ def read_image_collection(directory, regions, model)-> dict:
     for category in categories:
         if Path(category).is_dir():
             locations_map[locations_num]=category.split("/")[-1]
-            for train_file_name in glob.glob(category + "/*"):
+            for i,train_file_name in enumerate(glob.glob(category + "/*")):
                 if train_file_name.lower().split(".")[-1] in ["jpeg", "jpg", "png"]:
                     frame1 = cv2.imread(train_file_name)
                     if frame1.shape[-1] == 3:
                         dat = check(frame1, regions, model).flatten()
                         hashs = generate_hashs(dat)
-                        store_hashs(hashs, id1=locations_num, id2=1)
+                        store_hashs(hashs, id1=locations_num, id2=1+i)
             locations_num += 1
     return locations_map
 
